1e7d12c2d4294e2a3f51faa4966c612e596918b0
RussianBoy64 <kozlachkovvd@gmail.com>
2023-08-13 20:16:11 +0400

    feat: landing for desktop

--- f/js/common.js
+++ f/js/common.js
@@ -1,3 +1,26 @@
 window.addEventListener("DOMContentLoaded", initPage);
 
-function initPage() {}
+function initPage() {
+  initFaqAccordions();
+}
+
+function initFaqAccordions() {
+  const faqs = document.querySelectorAll(".faq__item");
+
+  for (let i = 0; i < faqs.length; i++) {
+    const toggleButton = faqs[i].querySelector(".faq__btn");
+    const answer = faqs[i].querySelector(".faq__answer");
+
+    function toggleFaqHandler() {
+      this.classList.toggle("faq__btn_open");
+
+      if (answer.style.maxHeight) {
+        answer.style.maxHeight = null;
+      } else {
+        answer.style.maxHeight = answer.scrollHeight + "px";
+      }
+    }
+
+    toggleButton.addEventListener("click", toggleFaqHandler);
+  }
+}
